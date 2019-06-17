625ae5f212fc5ffdc2de78fa92d267737c50a080
michael nthiwa <michaelnthiwa@michaels-MacBook-Pro-2.local>
2019-06-17 22:12:11 +0300

    bg(home page)sign up section:  - ensure that header and sign up section do not appear when a user signs up.
    [Deliver #166741630]

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -11,12 +11,11 @@ export class Home extends Component {
   render() {
     const {history} = this.props;
     const tokenData = JSON.parse(localStorage.getItem('jwt-data'));
-    console.log(tokenData);
     return (
       <div className={`home ${!tokenData ? 'not-authenticated': ''}`}>
         <NavBar isAuthenticated={tokenData}/>
         <Modal history={history}/>
-        <div className="intro-body">
+        <div className={`intro-body ${!tokenData ? 'active': 'inactive'}`}>
           <div className="intro">
             <span>The global community for audio culture</span>
             <small>Explore 15+ million radio shows, DJ mixes and podcasts made by passionate creators.</small>
